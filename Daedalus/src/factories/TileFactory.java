eff3af7e7eacb9ef403aa94dadccc91314b2768c
Lucas Yan <lucasyanog@gmail.com>
2025-09-24 20:28:23 -0300

    feat: adjusting TileFactory and adding Exit object

--- Daedalus/src/factories/TileFactory.java
+++ Daedalus/src/factories/TileFactory.java
@@ -1,5 +1,7 @@
 package factories;
 
+import java.util.List;
+
 import objects.Tile;
 
 public abstract class TileFactory {
@@ -9,18 +11,28 @@ public abstract class TileFactory {
                 return new objects.Parede(position);
             case '.':
                 return new objects.Chao(position);
-            case 'P':
-                return new objects.Porta(symbol, position);
             case 'S':
-                return new objects.Start(position); 
-            case 'p':
-                return new objects.Chave(symbol, position);
+                return new objects.Start(position);
+            case 'E':
+                return new objects.Exit(position);
             case 'T':
                 return new objects.Armadilha(position);
             case '$':
                 return new objects.Tesouro(position);
-            default:
-                throw new IllegalArgumentException("Símbolo desconhecido: " + symbol + " na posição " + position.toString());
-        }
+            }
+
+            List<Character> caracteresRestritos = List.of('S', 'E', 'T'); // Estes caracteres não representam portas ou chaves
+
+            char restrictedUpperSymbol = Character.toUpperCase(symbol);
+
+            if (Character.isUpperCase(symbol) && !caracteresRestritos.contains(restrictedUpperSymbol)) {
+                return new objects.Porta(symbol, position);
+            }
+            
+            if (Character.isLowerCase(symbol) && !caracteresRestritos.contains(restrictedUpperSymbol)) {
+                return new objects.Chave(symbol, position);
+            }
+
+            throw new IllegalArgumentException("Símbolo desconhecido: " + symbol + " na posição " + position.toString());
     }
 }
---
+++ Daedalus/src/objects/Exit.java
@@ -0,0 +1,10 @@
+package objects;
+
+import structures.Coordenada;
+
+public class Exit extends Porta {
+    public Exit(Coordenada position) {
+        super('E', position);
+    }
+  
+}
